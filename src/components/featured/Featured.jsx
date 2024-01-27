303559c3a1b3dab363b8fa1d899ec58ca83d54b4
geogrey12muiruri <greggambugua@gmail.com>
2024-01-27 10:05:22 -0800

    content modification

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -9,7 +9,7 @@ const Featured = () => {
         <section className="bg-white dark:bg-gray-900">
   <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
       <div className="max-w-screen-md mb-8 lg:mb-16">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Empowering the African Agricultural Sector: Real-time Prices, Insights, and Tools for Everyone</h2>
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Empowering the Kenyan Agricultural Sector: Real-time Prices, Insights, and Tools for Everyone</h2>
           <p className="text-gray-500 sm:text-xl dark:text-gray-400">Farmers, traders, consumers, policymakers, researchers, and investors - gain the crucial edge with Kenyas most comprehensive commodity price platform.</p>
       </div>
       <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
